e8238f9fbd99c8d62d358bbe5b92073aaa3f5d65
TheCoder5550 <alfons.nilsson@outlook.com>
2022-03-28 21:16:40 +0200

    new text

--- main.py
+++ main.py
@@ -5,14 +5,22 @@ from pygame.locals import*
 pygame.init() # intialize the library
 pygame.font.init()
 
-defaultFont = pygame.font.SysFont(None, 50)
-textSurface = defaultFont.render('Some Text', False, (0, 0, 0))
+BACKGROUND = (0, 0, 0)
+TEXTCOLOR = (255, 255, 255)
 
-# Set up the drawing window
+defaultFont = None
+headerFont = pygame.font.SysFont(defaultFont, 120)
+bodyFont = pygame.font.SysFont(defaultFont, 50)
+
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-# Run until the user asks to quit
+def drawText(font, text, pos, color):
+  textSurface = font.render(text, False, color)
+  screen.blit(textSurface, pos)
+
+tick = 0
 running = True
+
 while running:
     # Check if the user clicked the close button so that they dont keep playing for like
     # 2 years
@@ -21,19 +29,22 @@ while running:
             running = False
 
         if event.type == pygame.KEYDOWN:
-          if event.key == pygame.K_ESCAPE:
+          if event.key == pygame.K_TAB or event.key == pygame.K_ESCAPE:
             running = False
 
     # Fill the background with white (R, G, B) (also if you dont like lightmode, just replace it with (0,0,0))
-    screen.fill((255, 255, 255))
+    screen.fill(BACKGROUND)
+    pygame.draw.rect(screen, (20, 20, 20), pygame.Rect(0, 0, 320 * 2, 240 * 2))
 
     # Draw a blue circle in the center
     pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
 
-    screen.blit(textSurface, (0, 0))
+    drawText(headerFont, 'Dashboard', (10, 10), TEXTCOLOR)
+    drawText(bodyFont, 'Some Text ' + str(tick), (10, 130), TEXTCOLOR)
 
     # Flip the display
     pygame.display.flip()
+    tick += 1
 
 # Quit the program
 pygame.quit()
